4309976190e502d9c98058ae4510ac48023dd658
veetiveeti <veeti.v.reponen@gmail.com>
2020-09-14 14:26:54 +0300

    Matching password validation scripts & SCSS work
    – Script checks if "password" and "confirm-password" match when button "Sign Up" is activated.

---
+++ script.js
@@ -0,0 +1,22 @@
+// Checks if "password" and "confirm-password" match when button "Sign Up" is activated.
+
+const password = document.getElementById("password");
+const confirm_password = document.getElementById("confirm_password");
+
+function validatePassword() {
+    if(password.value != confirm_password.value) {
+        confirm_password.setCustomValidity("Passwords should match.");
+    }
+    else {
+        confirm_password.setCustomValidity("");
+    }
+}
+
+//password.onchange = validatePassword; <--Enable to see match on every change
+confirm_password.onkeyup = validatePassword;
+
+function ignoreValidatePassword() {
+    if(confirm_password = false) {
+        confirm_password.onkeyup = null;
+    }
+}
